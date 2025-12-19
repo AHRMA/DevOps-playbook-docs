fa21eecf27bb000f6729d13858c5e757313059d7
Amir Movahed <ahrmamir@gmail.com>
2025-12-19 08:09:00 +0330

    made a little hello world and write a documentation template for bashscripts

---
+++ Bash-script/hello.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#########################################################################
+# Script name	: Hello Bash
+# Description   : Sample and learing bash script
+# Author	: Amir Movaed 
+# Email		: ahrmamir@gmail.com
+# Date 		: Dec 19 2025
+# Version 	: v1.0.0
+#########################################################################
+
+echo "Hello World"
+
